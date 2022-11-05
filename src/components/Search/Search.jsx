c493be36d165ddf88e0cf5d24e84cbd3848dee9f
Artem Shchirov <artemshchirov@gmail.com>
2022-11-05 22:54:24 +0200

    feat: init adaptive

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,3 @@
-import Label from '../Label/Label';
-
 import './Search.scss';
 
 const Search = () => {
@@ -12,6 +10,19 @@ const Search = () => {
           placeholder="find a user"
         />
       </div>
+
+      <div className="chats__user">
+        <img
+          className="chats__avatar"
+          src="https://avatars.githubusercontent.com/u/78075439?v=4"
+          alt="#"
+        />
+        <div className="chats__info">
+          <span className="chats__username">ART</span>
+          <p className="chats__message">Hello</p>
+        </div>
+      </div>
+
     </div>
   );
 };
